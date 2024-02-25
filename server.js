087712f29b99080f5b5888943e62c5f710c1f2ff
Filipe Vieira dos Santos <filipevieiradossantos@gmail.com>
2024-02-25 16:06:37 -0300

    mudando a porta do server.js para 4848

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Define a porta do servidor como 443
-const PORT = process.env.PORT || 443;
+// Define a porta do servidor como 4848
+const PORT = process.env.PORT || 4848;
 
 // Inicia o servidor Node.js
 app.listen(PORT, () => {
