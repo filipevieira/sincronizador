b049044e0994c2d1fc036c0193c2d8bf3748ed18
Filipe Vieira dos Santos <filipevieiradossantos@gmail.com>
2024-02-25 14:31:00 -0300

    mudando a porta 8080 para 443

--- server.js
+++ server.js
@@ -10,8 +10,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Define a porta do servidor
-const PORT = process.env.PORT || 8080;
+// Define a porta do servidor como 443
+const PORT = process.env.PORT || 443;
+
+// Inicia o servidor Node.js
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
